dbc181dd246e55c1183f90bfeb0c2589426df787
Somnath474 <somnathgupta474@gmail.com>
2026-01-19 11:10:34 +0530

    Update script.js
    Signed-off-by: Somnath474 <somnathgupta474@gmail.com>

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 let gameSeq = [];
 let userSeq = [];
-
 let btns = ["yellow", "red", "purple", "green"];
 let started = false;
 let level = 0;
 let highestScore = 0;
 
 let h2 = document.querySelector("h2");
+let startBtn = document.getElementById("startBtn");
 
-// Start game
-document.addEventListener("keydown", function () {
+// Start game (keyboard or button)
+startBtn.addEventListener("click", startGame);
+document.addEventListener("keydown", startGame);
+
+function startGame() {
     if (!started) {
         started = true;
         levelUp();
+        startBtn.style.display = "none";
     }
-});
+}
 
+// Flash effects
 function gameFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(() => {
-        btn.classList.remove("flash");
-    }, 250);
+    setTimeout(() => btn.classList.remove("flash"), 250);
 }
 
 function userFlash(btn) {
     btn.classList.add("userflash");
-    setTimeout(() => {
-        btn.classList.remove("userflash");
-    }, 250);
+    setTimeout(() => btn.classList.remove("userflash"), 250);
 }
 
+// Next level
 function levelUp() {
     userSeq = [];
     level++;
@@ -43,6 +45,7 @@ function levelUp() {
     gameFlash(randBtn);
 }
 
+// Check user's input
 function checkAns(idx) {
     if (userSeq[idx] === gameSeq[idx]) {
         if (userSeq.length === gameSeq.length) {
@@ -51,22 +54,22 @@ function checkAns(idx) {
     } else {
         document.body.style.backgroundColor = "red";
         let finalScore = level - 1;
-
-        if (finalScore > highestScore) {
-            highestScore = finalScore;
-        }
+        if (finalScore > highestScore) highestScore = finalScore;
 
         setTimeout(() => {
             document.body.style.backgroundColor = "";
             h2.innerHTML = `Game Over! Your score was <b>${finalScore}</b><br>
             Highest Score: <b>${highestScore}</b><br>
-            Press any key to restart`;
+            Press "Start Game" or any key to restart`;
             resetGame();
         }, 200);
     }
 }
 
+// User button click
 function btnPress() {
+    if (!started) return;
+
     let btn = this;
     userFlash(btn);
 
@@ -76,14 +79,16 @@ function btnPress() {
     checkAns(userSeq.length - 1);
 }
 
-let allBtns = document.querySelectorAll(".btn");
-for (let btn of allBtns) {
+// Attach event listeners to all buttons
+document.querySelectorAll(".btn").forEach(btn => {
     btn.addEventListener("click", btnPress);
-}
+});
 
+// Reset game
 function resetGame() {
     started = false;
     gameSeq = [];
     userSeq = [];
     level = 0;
+    startBtn.style.display = "inline-block";
 }
